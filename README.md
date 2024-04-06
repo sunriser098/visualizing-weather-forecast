c516bb876f4289f8113d46ee29e2d7fb8002ac04
sunriser <sunriser098@gmail.com>
2024-04-06 13:26:54 -0500

    Add readme file, plot dummy data dynamically, and add functions file to maintain methods.

---
+++ README.md
@@ -0,0 +1,5 @@
+## What is this Project about?
+
+- Building a data web app that visualizes the weather forecast for the next five days of any city
+- Plotly libraries for data visualization
+- Python Streamlit for web app
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import plotly.express as px
+import functions as f
+
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -6,3 +9,9 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} in {place}")
+
+
+d, t = f.get_data(days)
+figure = px.line(x=d, y=t,
+                 labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
